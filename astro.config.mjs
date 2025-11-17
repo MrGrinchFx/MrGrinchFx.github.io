39ad219707dbdce07b5e7b86789a7778138abfc7
Kevin Le <2003kevinle@gmail.com>
2025-11-17 13:15:30 -0800

    added back base address

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
   site: "https://Kevin-Le.github.io",
+  base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
