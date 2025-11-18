e9e4f3225829fe64e2b4596b437583b3a91dede3
Kevin Le <2003kevinle@gmail.com>
2025-11-18 14:27:11 -0800

    fixed license

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Kevin-Le.github.io",
+  site: "https://mrgrinchfx.github.io",
   base: "/",
 	trailingSlash: "always",
 	integrations: [
